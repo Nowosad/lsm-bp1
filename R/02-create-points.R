32b50e1211f2ec7c11a37d56358be252a47004a1
jn <tupiszakaczki@gmail.com>
2019-02-06 21:04:04 +0100

    saves points to csv

--- R/02-create-points.R
+++ R/02-create-points.R
@@ -6,4 +6,14 @@ my_map = mapview(r)
 my_points = mapedit::drawFeatures(my_map)
 # plot(r)
 # plot(st_geometry(my_points), add = TRUE)
-st_write(my_points, "data/sample_points.gpkg")
+st_write(my_points, "raw-data/sample_points.gpkg")
+
+# points to csv -----------------------------------------------------------
+library(sf)
+library(dplyr)
+my_points = st_read("raw-data/sample_points.gpkg", quiet = TRUE) %>% 
+  st_coordinates() %>% 
+  as_tibble() %>% 
+  mutate(siteID = 1:3) %>% 
+  rename(longitude = X, latitude = Y) %>% 
+  write.csv("data/sample_points.csv")
